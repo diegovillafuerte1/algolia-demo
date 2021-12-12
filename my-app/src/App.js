951d08db5ef8ad49ac2d51ba63faf0adfcae8fba
Diego V <diegovillafuerte1@gmail.com>
2021-12-12 11:02:58 -0800

    use wine dataset from personal dashboard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
+// const searchClient = algoliasearch(
+//   'latency',
+//   '6be0576ff61c053d5f9a3225e2a90f76'
+// );
+
 const searchClient = algoliasearch("1DDP2SO5ID", "97fa4f0a39cda3d338bc74dc2d4214ff");
+// const index = client.initIndex("wine");
+
+// const objects = [
+//   {
+//     objectID: 1,
+//     name: "Foo"
+//   }
+// ];
+
+// index
+//   .saveObjects(objects)
+//   .then(({ objectIDs }) => {
+//     console.log(objectIDs);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 function App() {
   return (
