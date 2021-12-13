a6fb8d2c8236580c4707ee765895e2818fd1f1b2
Diego V <diegovillafuerte1@gmail.com>
2021-12-12 17:03:43 -0800

    mvp, should do some more styling, adding of cooler agolia features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import React, { Component } from 'react';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+  SortBy,
+  Rheostat
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import './App.css';
 
 const searchClient = algoliasearch("1DDP2SO5ID", "97fa4f0a39cda3d338bc74dc2d4214ff");
 
-function App() {
+class App extends Component {
+  render() {
+    return (
+      <div className="ais-InstantSearch">
+        <InstantSearch indexName="wine" searchClient={searchClient}>
+          <div className="ais-SearchZone">
+            <div className="title-text">Wine Search</div>
+            <SearchBox />
+          </div>
+          <div className="ais-PanelZone">
+            <div className="left-panel">
+              <ClearRefinements />
+              <h2>Region</h2>
+              <RefinementList attribute="domain" />
+              {/* <Rheostat /> */}
+              {/* <SortBy
+                items={[{label: 'Price (asc)', value: 'wine_price_asc'}]}
+              /> */}
+              <Configure hitsPerPage={8} />
+            </div>
+            <div className="right-panel">
+              <Hits hitComponent={Hit} />
+              <Pagination />
+            </div>
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-      <InstantSearch
-        indexName="wine"
-        searchClient={searchClient}
-      >
-        <SearchBox />
-        <Hits />
-      </InstantSearch>
-      </header>
+    <div>
+      <img src={props.hit.image} align="left" alt={props.hit.name} />
+      <div className="hit-name">
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-domain">{props.hit.domain}</div>
+      <div className="hit-year">{props.hit.year}</div>
+      <div className="hit-quality">{props.hit.quality}</div>
+      <div className="hit-type">{props.hit.type}</div>
+      <div className="hit-price">€{props.hit.price}</div>
     </div>
   );
 }
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default App;
